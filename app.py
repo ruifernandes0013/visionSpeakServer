293915c3fafcc545c24d54e3fd4b2801080c2191
Rui Fernandes <rui.fernandes@craftablesoftware.com>
2024-02-23 14:34:26 +0000

    v1.0.0

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+from io import BytesIO
 from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 import torch
-from PIL import Image
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app, origins="*", methods=["GET", "POST"], headers=["Content-Type"])
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
@@ -13,23 +18,52 @@ max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
-def predict_step(image_paths):
-  images = []
-  for image_path in image_paths:
-    i_image = Image.open(image_path)
-    if i_image.mode != "RGB":
-      i_image = i_image.convert(mode="RGB")
+def predict_step(image):
+    i_image = image.convert(mode="RGB")
 
-    images.append(i_image)
+    pixel_values = feature_extractor(images=[i_image], return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
 
-  pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-  pixel_values = pixel_values.to(device)
+    output_ids = model.generate(pixel_values, **gen_kwargs)
 
-  output_ids = model.generate(pixel_values, **gen_kwargs)
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
 
-  preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-  preds = [pred.strip() for pred in preds]
-  return preds
+@app.route('/', methods=['GET'])
+def ping():
+    return 'API is running...'
+    
+@app.route('/api/photo', methods=['POST'])
+def predict_photo():
+    try:
+        # Check if the POST request has the file part
+        if 'file' not in request.files:
+            return jsonify({"error": "No file part"}), 400
+        
+        file = request.files['file']
 
-result = predict_step(['elonmusk.jpg'])
-print(result)
+        # If the user does not select a file, the browser may submit an empty file without a filename
+        if file.filename == '':
+            return jsonify({"error": "No selected file"}), 400
+
+        # If the file exists and is an image
+        if file:
+            # Read the image file
+            contents = file.read()
+            
+            # Open image from bytes
+            image = Image.open(BytesIO(contents))
+            
+            # Predict caption for the image
+            result = predict_step(image)
+            
+            # Return the predicted caption as JSON response
+            return result[0], 200
+        else:
+            return jsonify({"error": "Invalid file"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(port=3002)
